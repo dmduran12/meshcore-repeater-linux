10c881de8a8fe344f63254f8cfd68cd8bc714d58
dmduran12 <dmduran@gmail.com>
2025-12-12 10:13:33 -0800

    Fix import: StatsApp -> APIApp in __init__.py

--- repeater/web/__init__.py
+++ repeater/web/__init__.py
@@ -1,10 +1,10 @@
-from .http_server import HTTPStatsServer, StatsApp, LogBuffer, _log_buffer
+from .http_server import HTTPStatsServer, APIApp, LogBuffer, _log_buffer
 from .api_endpoints import APIEndpoints
 from .cad_calibration_engine import CADCalibrationEngine
 
 __all__ = [
     'HTTPStatsServer',
-    'StatsApp', 
+    'APIApp', 
     'LogBuffer',
     'APIEndpoints',
     'CADCalibrationEngine',
